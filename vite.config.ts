ba56330a26598f5b12b5dc8890ac7ef029da7943
Kawin wanthongkum <2610711342001@live4.utcc.ac.th>
2026-09-18 23:10:57 +0700

    Modify Vite config to remove tailwindcss
    Removed tailwindcss plugin and updated base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs.plugin-react'
 
-export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
+export default defineConfig({
+  plugins: [react()],
+  base: '/SyntaxStrike.github.io/', // ต้องตรงกับชื่อ Repository ของคุณ
+})
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
